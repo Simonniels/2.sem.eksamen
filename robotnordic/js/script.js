17e06d1073c228d7312956dbf6f8e14b11181fc9
kasim kara <45227174+turkaka@users.noreply.github.com>
2020-05-11 09:20:15 +0200

    kk

--- robotnordic/js/script.js
+++ robotnordic/js/script.js
@@ -24,42 +24,34 @@ window.onclick = function(event) {
 
 function buttonFunction() {
   document.getElementById("button").classList.toggle("choose");
+  document.getElementById("button1").classList.remove("choose");
+  document.getElementById("button2").classList.remove("choose");
+  document.getElementById("button3").classList.remove("choose");
+
 }
-window.onclick = function(event) {
-  if (!event.target.matches('.button0')) {
-    var button = document.getElementsByClassName("button0");
-    var i;
-    for (i = 0; i < button.length; i++) {
-      var openDropdown = button[i];
-      if (openDropdown.classList.contains('choose')) {
-        openDropdown.classList.remove('choose');
-      }
-    }
-  }
-}
-
-
-
 
 
 function buttonFunction1() {
   document.getElementById("button1").classList.toggle("choose");
+  document.getElementById("button").classList.remove("choose");
+  document.getElementById("button2").classList.remove("choose");
+  document.getElementById("button3").classList.remove("choose");
 }
-window.onclick = function(event) {
-  if (!event.target.matches('.button1')) {
 
-      if (openDropdown.classList.contains('choose')) {
-        openDropdown.classList.remove('choose');
-      }
-    }
-  }
 
 function buttonFunction2() {
   document.getElementById("button2").classList.toggle("choose");
+  document.getElementById("button1").classList.remove("choose");
+  document.getElementById("button").classList.remove("choose");
+  document.getElementById("button3").classList.remove("choose");
 }
 function buttonFunction3() {
   document.getElementById("button3").classList.toggle("choose");
+  document.getElementById("button1").classList.remove("choose");
+  document.getElementById("button2").classList.remove("choose");
+  document.getElementById("button").classList.remove("choose");
 }
 function buttonFunction4() {
   document.getElementById("button4").classList.toggle("choose");
+
 }
