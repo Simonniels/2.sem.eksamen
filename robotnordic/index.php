99f632e5afca9624044027a06387d9ead3b8a997
kasim kara <45227174+turkaka@users.noreply.github.com>
2020-05-18 12:27:30 +0200

    kk

--- robotnordic/index.php
+++ robotnordic/index.php
@@ -2,7 +2,42 @@
 
 <?php include "header.html"; ?>
 
-<?php include "main.html"; ?>
+
+  <main>
+
+
+      <section class="full">
+
+        <div class="button button1" onclick="buttonFunction1()"  id="button1" >
+
+        <h2>Konto</h2>
+        </div>
+
+          </section>
+
+          <section class="half">
+
+                <div class="button button0" onclick="buttonFunction()"  id="button" >
+                <img src="image/icon/box.png" alt="icon">
+                <h2>Gemt</h2>
+
+                </div>
+
+          </section>
+
+        <section class="half">
+
+          <div class="button button2" onclick="buttonFunction2()"  id="button2" >
+    <a href="ny.html">
+            <img src="image/icon/palle.png" alt="icon">
+            <h2>Tilføj ny varer</h2></a>
+          </div>
+
+
+        </section>
+
+
+    </main>
 
   </body>
 </html>
