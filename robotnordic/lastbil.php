b28b83eee41dcb79700067e5a55ae845a494de8c
kasim kara <45227174+turkaka@users.noreply.github.com>
2020-05-28 00:26:30 +0200

    kk

--- robotnordic/lastbil.php
+++ robotnordic/lastbil.php
@@ -4,38 +4,27 @@
   <main>
 
           <section class="full info">
-
             <h1>Vælg Lastbil Højde</h1>
-
               </section>
 
         <section id="lastbil"></section>
 
-
-
         <section class="half">
-
           <div class="button udenicon">
             <h3>Indtaste speciel høj</h3>
-
               <form action="overview-lastbil.php"  method="get">
               <input type="text" name="lastbil">
               <input type="submit">
             </form>
-
-
           </div>
-
         </section>
 
         <section class="full">
-
             <div class="button">
                 <button onclick="goBack()">
               <h2>Tilbage</h2>
               </button>
             </div>
-
         </section>
 
     </main>
@@ -44,7 +33,7 @@
 
 /* Lasbil siden. Det viser forskerlige lastbil højt oplysninger. */
 var lastbil, text, lastbilLen, i;
-lastbil = ["2.70", "2.75", "2.80", "2.85", "2.90","2.95"];
+lastbil = ["2.70", "2.75", "2.80", "2.85"];
 lastbilLen = lastbil.length;
 text="";
 for (i = 0; i < lastbilLen; i++) {
